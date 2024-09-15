78b8827b2a81cacc65cdb436e9435a8b252fd6d9
Amelia Sinclaire <asinclaire.hri@gmail.com>
2024-09-15 16:10:44 -0400

    updated tests

--- guess_syllables/syllables_test.py
+++ guess_syllables/syllables_test.py
@@ -35,21 +35,21 @@ def main():
     test_func(regex_syllables, word_list)
     test_func(sammy_guess, word_list)
     test_func(syllables.estimate, word_list)
-    test_func(guess_syllables, word_list)
+    test_func(num_syllables, word_list)
 
     print('---Test on words only in CMU---')
     test_func(phone_syllables, cmu_word_list)
     test_func(regex_syllables, cmu_word_list)
     test_func(sammy_guess, cmu_word_list)
     test_func(syllables.estimate, cmu_word_list)
-    test_func(guess_syllables, cmu_word_list)
+    test_func(num_syllables, cmu_word_list)
 
     print('---Test on non-CMU words---')
     test_func(phone_syllables, non_cmu)
     test_func(regex_syllables, non_cmu)
     test_func(sammy_guess, non_cmu)
     test_func(syllables.estimate, non_cmu)
-    test_func(guess_syllables, non_cmu)
+    test_func(num_syllables, non_cmu)
 
 if __name__ == '__main__':
     main()
--- tests.py
+++ tests.py
@@ -13,37 +13,40 @@ class TestSequenceFunctions(unittest.TestCase):
         self.hd = haiku.HaikuDetector()
 
     def test_syllables(self):
-        self.assertIn(1, [sum(x) for x in self.hd.num_syllables("dog")])
-        self.assertIn(1, [sum(x) for x in self.hd.num_syllables("asdf")])
-        self.assertIn(2, [sum(x) for x in self.hd.num_syllables("letter")])
-        self.assertIn(3, [sum(x) for x in self.hd.num_syllables("washington")])
-        self.assertIn(1, [sum(x) for x in self.hd.num_syllables("dock")])
-        self.assertIn(2, [sum(x) for x in self.hd.num_syllables("dangle")])
-        self.assertIn(1, [sum(x) for x in self.hd.num_syllables("thrive")])
-        self.assertIn(1, [sum(x) for x in self.hd.num_syllables("fly")])
-        self.assertIn(2, [sum(x) for x in self.hd.num_syllables("placate")])
-        self.assertIn(2, [sum(x) for x in self.hd.num_syllables("renege")])
-        self.assertIn(3, [sum(x) for x in self.hd.num_syllables("reluctant")])
+        self.assertIn(1, [sum(x) for x in self.hd.num_syllables_line("dog")])
+        self.assertIn(1, [sum(x) for x in self.hd.num_syllables_line("asdf")])
+        self.assertIn(2, [sum(x) for x in self.hd.num_syllables_line("letter")])
+        self.assertIn(3, [sum(x) for x in self.hd.num_syllables_line("washington")])
+        self.assertIn(1, [sum(x) for x in self.hd.num_syllables_line("dock")])
+        self.assertIn(2, [sum(x) for x in self.hd.num_syllables_line("dangle")])
+        self.assertIn(1, [sum(x) for x in self.hd.num_syllables_line("thrive")])
+        self.assertIn(1, [sum(x) for x in self.hd.num_syllables_line("fly")])
+        self.assertIn(2, [sum(x) for x in self.hd.num_syllables_line("placate")])
+        self.assertIn(2, [sum(x) for x in self.hd.num_syllables_line("renege")])
+        self.assertIn(3, [sum(x) for x in self.hd.num_syllables_line("reluctant")])
 
-        self.assertIn(2, [sum(x) for x in self.hd.num_syllables("firelight")])
-        self.assertIn(2, [sum(x) for x in self.hd.num_syllables("frolicked")])
-        self.assertIn(3, [sum(x) for x in self.hd.num_syllables("doomscrolling")])
-        self.assertIn(3, [sum(x) for x in self.hd.num_syllables("treasuring")])
-        self.assertIn(3, [sum(x) for x in self.hd.num_syllables("re echoes")])
-        self.assertIn(2, [sum(x) for x in self.hd.num_syllables("gunna")])
-        self.assertIn(3, [sum(x) for x in self.hd.num_syllables("unresolve")])
-        self.assertIn(2, [sum(x) for x in self.hd.num_syllables("bloodshot")])
-        self.assertIn(3, [sum(x) for x in self.hd.num_syllables("tempura")])
-        self.assertIn(5, [sum(x) for x in self.hd.num_syllables("ozymandias")])
+        self.assertIn(3, [sum(x) for x in self.hd.num_syllables_line("firelight")])
+        self.assertIn(2, [sum(x) for x in self.hd.num_syllables_line("frolicked")])
+        self.assertIn(3, [sum(x) for x in self.hd.num_syllables_line("doomscrolling")])
+        self.assertIn(3, [sum(x) for x in self.hd.num_syllables_line("treasuring")])
+        self.assertIn(3, [sum(x) for x in self.hd.num_syllables_line("re echoes")])
+        self.assertIn(2, [sum(x) for x in self.hd.num_syllables_line("gunna")])
+        self.assertIn(3, [sum(x) for x in self.hd.num_syllables_line("unresolve")])
+        self.assertIn(2, [sum(x) for x in self.hd.num_syllables_line("bloodshot")])
+        self.assertIn(3, [sum(x) for x in self.hd.num_syllables_line("tempura")])
+        self.assertIn(5, [sum(x) for x in self.hd.num_syllables_line("ozymandias")])
 
     def test_haikus(self):
         hd = haiku.HaikuDetector()
         with open('haiku_list.json') as f:
             haiku_list = json.load(f)
+        results = []
         for poem in haiku_list:
             # lines = poem.split('/')
             # lines = [x for x in lines if (x != '' and x != ' ')]
-            self.assertTrue(hd.is_haiku(poem))
+            results.append(hd.is_haiku(poem))
+        accuracy = 100 * results.count(True) / len(results)
+        print(f'{results.count(True)} / {len(results)} ({accuracy:.2f}%)')
 
 
 if __name__ == '__main__':
