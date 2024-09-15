913308b05012c13f14de4a1135cf1ea4d0a313a4
Amelia Sinclaire <asinclaire.hri@gmail.com>
2024-09-15 15:45:59 -0400

    Added in test on non-CMU words

--- guess_syllables/syllables_test.py
+++ guess_syllables/syllables_test.py
@@ -8,6 +8,7 @@ from guess_syllables import *
 with open('syllable_dictionary.json') as f:
     word_list = json.load(f)
 cmu_word_list = dict([(w, c) for w, c in word_list.items() if w in pronunciations])
+non_cmu = {k:v for k,v in word_list.items() if k not in cmu_word_list or v != cmu_word_list[k]}
 
 def list_wrap(something):
     return something if type(something) is list else [something]
@@ -29,6 +30,7 @@ def test_func(func, word_list):
     print()
 
 def main():
+    print('---Test on all words---')
     test_func(phone_syllables, word_list)
     test_func(regex_syllables, word_list)
     test_func(sammy_guess, word_list)
@@ -41,6 +43,13 @@ def main():
     test_func(sammy_guess, cmu_word_list)
     test_func(syllables.estimate, cmu_word_list)
     test_func(guess_syllables, cmu_word_list)
+
+    print('---Test on non-CMU words---')
+    test_func(phone_syllables, non_cmu)
+    test_func(regex_syllables, non_cmu)
+    test_func(sammy_guess, non_cmu)
+    test_func(syllables.estimate, non_cmu)
+    test_func(guess_syllables, non_cmu)
 
 if __name__ == '__main__':
     main()
